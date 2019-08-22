96e7d83981e6617e354b70c2810212b12317b5c0
MichelleLanzinger <42041831+MichelleLanzinger@users.noreply.github.com>
2019-08-22 18:24:10 +0200

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     int themeColor;
     int appColor;
 
+//dies ist ein test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
