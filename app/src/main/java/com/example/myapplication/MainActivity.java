bc0d4a1aa2f3ac1075609d996e1c20e2c38b3ecc
3qic <41987661+3qic@users.noreply.github.com>
2019-08-30 17:26:01 +0200

    Notifunktion ist komplett fertig. libary für Datenbank ist implementiert

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -154,6 +154,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         addbutton.setVisibility(View.INVISIBLE);
     }
 
-
-
 }
--- app/src/main/java/com/example/myapplication/NoteFragment.java
+++ app/src/main/java/com/example/myapplication/NoteFragment.java
@@ -1,27 +1,53 @@
 package com.example.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.OutputStreamWriter;
 
 public class NoteFragment extends Fragment {
-    EditText Note;
-    Button saveButton;
+    EditText Notepad;
+    FloatingActionButton saveButton;
+    String textnote;
+    SharedPreferences.Editor editor;
+    SharedPreferences preferences;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View v = inflater.inflate(R.layout.fragment_notes, container, false);
 
+        Notepad = v.findViewById(R.id.EditTextNote);
+        saveButton = v.findViewById(R.id.floating_save_button);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        textnote = preferences.getString("Text", "");
+        Notepad.setText(textnote);
+        editor = preferences.edit();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textnote = Notepad.getText().toString();
+                editor.putString("Text", textnote);
+                editor.commit();
+
+                Toast.makeText(getContext(), "Notiz gespeichert", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return v;
     }
 
 
